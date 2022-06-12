bc4ef4828738f77645617c3fc551780c4e7f0836
pablitar <pablitar@gmail.com>
2022-06-12 19:52:50 -0300

    actualizado el ejemplo hasta lo que debería ser el final de la primera sesión

--- player/Player.gd
+++ player/Player.gd
@@ -1,18 +1,33 @@
 extends KinematicBody2D
 
-var speed = 60
+var velocity: Vector2 = Vector2.ZERO
 
-var moving = false
-
-func _ready():
-	print("Hola Mundo")
+export(float) var move_speed: float = 100
+export(float) var gravity: float = 400
+export(float) var jump_speed: float = 250
 
 func _physics_process(delta):
 	var movement_direction = Vector2.ZERO
 	
-	if Input.is_key_pressed(KEY_RIGHT):
+	if Input.is_action_pressed("move_right"):
 		movement_direction = Vector2.RIGHT
-	if Input.is_key_pressed(KEY_LEFT):
+		$PlayerSprites.flip_h = false
+	if Input.is_action_pressed("move_left"):
 		movement_direction = Vector2.LEFT
+		$PlayerSprites.flip_h = true
+	
+	if is_on_floor():
+		if movement_direction != Vector2.ZERO:
+			$PlayerSprites.play("walk")
+		else:
+			$PlayerSprites.play("idle")
+	else:
+		$PlayerSprites.play("jump")
 		
-	move_and_slide(movement_direction * speed)
+	if Input.is_action_just_pressed("jump") && is_on_floor():
+		velocity.y -= jump_speed
+		
+	velocity.x = movement_direction.x * move_speed
+	velocity.y = velocity.y + gravity * delta
+		
+	velocity = move_and_slide(velocity, Vector2.UP)
